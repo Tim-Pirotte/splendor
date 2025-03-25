2e2dc7edd321d51cf68c21542fef6c2df85a4b88
niels clarysse <niels.clarysse@student.howest.be>
2025-03-25 11:11:51 +0100

    Partialy implements the ability to retreve and save the username form the form by adding an event listener and simple if-statement

--- src/assets/js/main-menu-component/index.js
+++ src/assets/js/main-menu-component/index.js
@@ -1,7 +1,8 @@
-import {loadFromStorage} from "../data-connector/local-storage-abstractor";
+import {loadFromStorage} from "../data-connector/local-storage-abstractor.js";
 
 function init() {
     loadUsername();
+    document.querySelector(".form-actions").addEventListener("click", storeUsername)
 }
 
 function loadUsername() {
@@ -9,6 +10,15 @@ function loadUsername() {
     const username = loadFromStorage("username");
 
     if (username) { $username.value = username }
+}
+
+function storeUsername(e) {
+    e.preventDefault()
+
+    const valueList = ["join-game", "create-game"];
+    if (valueList.includes(e.target.value)) {
+        console.log("hello world!");
+    }
 }
 
 init();
