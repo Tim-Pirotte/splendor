8d47df264cbe2c497f5406f2374382c7beef2983
niels clarysse <niels.clarysse@student.howest.be>
2025-03-25 10:41:08 +0100

    Adds the function and imports needed to load the username form local storage (if precent)

--- src/assets/js/main-menu-component/index.js
+++ src/assets/js/main-menu-component/index.js
@@ -1,5 +1,14 @@
-function init() {
+import {loadFromStorage} from "../data-connector/local-storage-abstractor";
 
+function init() {
+    loadUsername();
+}
+
+function loadUsername() {
+    const $username = document.querySelector("#username");
+    const username = loadFromStorage("username");
+
+    if (username) { $username.value = username }
 }
 
 init();
