1a876e160b212d6a8a2bde9955f3f0cfef40d259
niels clarysse <niels.clarysse@student.howest.be>
2025-03-25 15:45:26 +0100

    Reformates the index.html to work with helper.js file (to make it easyer, new addition will otherwise make the index.js file to large)

--- src/assets/js/main-menu-component/index.js
+++ src/assets/js/main-menu-component/index.js
@@ -1,29 +1,13 @@
-import {loadFromStorage, saveToStorage} from "../data-connector/local-storage-abstractor.js";
+import {loadAvatar, loadUsername, storeUsername} from "./helper.js";
 
 function init() {
-    loadUsername();
+    loadUserInformation();
     document.querySelector(".form-actions").addEventListener("click", storeUsername);
 }
 
-function loadUsername() {
-    const $username = document.querySelector("#username");
-    const username = loadFromStorage("username");
-
-    if (username) {
-        $username.value = username;
-    }
-}
-
-function storeUsername(e) {
-    e.preventDefault();
-
-    const valid = document.querySelector("form").reportValidity();
-    const username = document.querySelector("#username").value;
-
-    if (valid && (username.trim() !== "")) {
-        saveToStorage("username", username);
-        window.location.href = `./pages/${e.target.value}.html`;
-    }
+function loadUserInformation() {
+    loadUsername();
+    loadAvatar();
 }
 
 init();
