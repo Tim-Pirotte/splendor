95e8eca0dfefe0addb062f4c515e04c115294893
Tim Pirotte <timpirotte@outlook.com>
2025-03-25 17:56:41 +0100

    Render other players developments

--- src/assets/js/board/renderer.js
+++ src/assets/js/board/renderer.js
@@ -29,6 +29,7 @@ function renderOtherPlayers(otherPlayers) {
     $playerCard.querySelector(".points").textContent = otherPlayer.totalPrestigePoints;
 
     renderTokenList($playerCard.querySelector(".tokens"), otherPlayer.tokens);
+    renderCardList($playerCard.querySelector(".cards"), otherPlayer.bonuses);
 
     $otherPlayerContainer.appendChild($playerCard);
   }
@@ -42,10 +43,28 @@ function renderTokenList(containerToInsertInto, tokenAmounts) {
     const $token = $numberedItemTemplate.content.firstElementChild.cloneNode(true);
     const $image = $imageTemplate.content.firstElementChild.cloneNode(true);
     $token.querySelector(".amount").textContent = tokenAmounts[token] || 0;
-    $image.querySelector("source").srcset = "../assets/images/UI/tokens/" + mapTokens[token] + "_chip.webp"
-    $image.querySelector("img").src = "../assets/images/fallback/UI/tokens/black_chip.png"
+    $image.querySelector("source").srcset = "../assets/images/UI/tokens/" + mapTokens[token] + "_chip.webp";
+    $image.querySelector("img").src = "../assets/images/fallback/UI/tokens/" + mapTokens[token] + "_chip.png";
     $token.insertAdjacentHTML("beforeend", $image.outerHTML);
     containerToInsertInto.appendChild($token);
+  }
+}
+
+function renderCardList(containerToInsertInto, cardAmounts) {
+  const $numberedItemTemplate = document.querySelector("#numbered-item-template");
+  const $imageTemplate = document.querySelector("#image-template");
+
+  for (const cardType of tokensDummyData.gems) {
+    if (cardType !== "Gold") {
+      const $card = $numberedItemTemplate.content.firstElementChild.cloneNode(true);
+      const $image = $imageTemplate.content.firstElementChild.cloneNode(true);
+      $card.querySelector(".amount").textContent = cardAmounts[cardType] || 0;
+      $image.querySelector("source").srcset = "../assets/images/UI/cards/" + mapTokens[cardType] + "_card_small.webp";
+      $image.querySelector("img").src = "../assets/images/fallback/UI/cards/" + mapTokens[cardType] + "_card_small.png";
+
+      $card.insertAdjacentHTML("beforeend", $image.outerHTML);
+      containerToInsertInto.appendChild($card);
+    }
   }
 }
 
