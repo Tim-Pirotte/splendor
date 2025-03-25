de8b805c3eeed8af4d1f3a313b2c88b15d8f2c66
Tim Pirotte <timpirotte@outlook.com>
2025-03-25 15:49:37 +0100

    Adds the player names to the sidebar

--- src/assets/js/board/board.js
+++ src/assets/js/board/board.js
@@ -2,8 +2,8 @@ import {dummyData} from "./dummy-data.js";
 import {renderPage} from "./renderer.js";
 
 function init() {
-  console.log(dummyData)
-  renderPage()
+  console.log(dummyData);
+  renderPage(dummyData);
 }
 
 init();
--- src/assets/js/board/renderer.js
+++ src/assets/js/board/renderer.js
@@ -1,11 +1,24 @@
 import {loadFromStorage} from "../data-connector/local-storage-abstractor.js";
 
-function renderPage() {
+function renderPage(gameData) {
   renderClientUser();
+  renderOtherPlayers(gameData.players);
 }
 
 function renderClientUser() {
   document.querySelector(".top-bar h2").textContent = loadFromStorage("username")
+}
+
+function renderOtherPlayers(otherPlayers) {
+  const $otherPlayerContainer = document.querySelector(".other-players");
+  const $template = document.querySelector("#other-player-card-template");
+
+  for (const otherPlayer of otherPlayers) {
+    const $playerCard = $template.content.firstElementChild.cloneNode(true);
+    $playerCard.querySelector(".name").textContent = otherPlayer.name;
+
+    $otherPlayerContainer.appendChild($playerCard);
+  }
 }
 
 export { renderPage };
