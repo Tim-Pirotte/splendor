e9a07a338d1805f6e42a240db0f6cf886d7003db
niels clarysse <niels.clarysse@student.howest.be>
2025-03-25 16:33:49 +0100

    Adds the option to render the avatars as fallback

--- src/assets/js/main-menu-component/renderer.js
+++ src/assets/js/main-menu-component/renderer.js
@@ -8,6 +8,9 @@ function renderSelectableAvatars() {
     for (const avatar of avatars) {
         const $picture = $template.content.firstElementChild.cloneNode(true);
         $picture.querySelector("source").setAttribute("srcset", `./assets/images/avatars/${avatar}.webp`);
+        $picture.querySelector("img").setAttribute("src", `./assets/fallback/avatars/${avatar}.png`);
+        $picture.querySelector("img").setAttribute("alt", `${avatar}`);
+        $picture.querySelector("img").setAttribute("title", `${avatar}.`);
         $avatars.insertAdjacentHTML("beforeend", $picture.outerHTML);
     }
 }
