51293115342d881ba108616db272346acf93b8ba
niels clarysse <niels.clarysse@student.howest.be>
2025-03-26 12:13:17 +0100

    Adds the api fetching

--- src/assets/js/join-game-component/renderer.js
+++ src/assets/js/join-game-component/renderer.js
@@ -1,16 +1,16 @@
-import {getAmountText, getGameButtonText} from "./helper.js";
 import * as objectHandler from "./object-handler.js";
-import {fetchFromServer} from "../data-connector/api-communication-abstractor";
+import {getAmountText, getGameButtonText} from "./helper.js";
+import {fetchFromServer} from "../data-connector/api-communication-abstractor.js";
 
 function renderList(){
     const $template = document.querySelector("#game-template");
     const $container = document.querySelector("ul");
-    //const gameObject = fetchFromServer();
 
-    document.querySelectorAll("li")
-        .forEach(li => li.remove());
+    $container.innerHTML = "";
 
-    gameObject['games'].forEach(game => populateGame($template, $container, game));
+    fetchFromServer("/games")
+        .then(gameObject => gameObject['games'].forEach(game => populateGame($template, $container, game)))
+        .catch(error => console.error(error));
 }
 
 function populateGame($template, $container, game){
