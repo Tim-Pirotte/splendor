f4f908fe748e466e94b738e529b635ae5f1a90df
Tim Pirotte <timpirotte@outlook.com>
2025-03-26 14:25:37 +0100

    Styles the code

--- src/assets/js/board-component/game-data-handler.js
+++ src/assets/js/board-component/game-data-handler.js
@@ -3,8 +3,8 @@ import {fetchFromServer} from "../data-connector/api-communication-abstractor.js
 import {renderPage} from "./renderer/renderer.js";
 
 function handleGameDataError(err) {
-  console.log(err);
   const statusCode = err["failure"];
+
   if (statusCode === 403 || statusCode === 401) {
     location.href = "../index.html";
   }
@@ -13,6 +13,7 @@ function handleGameDataError(err) {
 function updateGameData() {
   const gameId = loadFromStorage("gameId");
   if (gameId === null) location.href = "../index.html";
+
   fetchFromServer(`/games/${gameId}`)
     .then(gameData => renderPage(gameData))
     .catch(err => handleGameDataError(err));
