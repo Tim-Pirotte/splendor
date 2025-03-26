36f63658024aa8a18f17c4f3bddc344cbc718fe3
Tim Pirotte <timpirotte@outlook.com>
2025-03-26 14:20:46 +0100

    Fetches the game data from the server

--- src/assets/js/board-component/game-data-handler.js
+++ src/assets/js/board-component/game-data-handler.js
@@ -2,10 +2,20 @@ import {loadFromStorage} from "../data-connector/local-storage-abstractor.js";
 import {fetchFromServer} from "../data-connector/api-communication-abstractor.js";
 import {renderPage} from "./renderer/renderer.js";
 
+function handleGameDataError(err) {
+  console.log(err);
+  const statusCode = err["failure"];
+  if (statusCode === 403 || statusCode === 401) {
+    location.href = "../index.html";
+  }
+}
+
 function updateGameData() {
-  const gameId = loadFromStorage("GameId");
-  if (gameId === null) {}
-  fetchFromServer(`/games/${gameId}`).then(res => res.json()).then(res => renderPage(res));
+  const gameId = loadFromStorage("gameId");
+  if (gameId === null) location.href = "../index.html";
+  fetchFromServer(`/games/${gameId}`)
+    .then(gameData => renderPage(gameData))
+    .catch(err => handleGameDataError(err));
 }
 
 export {updateGameData};
