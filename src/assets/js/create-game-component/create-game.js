e9f252d59d12109b92577ded8f45033dd4fa52b8
Arne Persyn <arnepersyn8@gmail.com>
2025-03-26 13:28:25 +0100

    Created a function that form the body

--- src/assets/js/create-game-component/create-game.js
+++ src/assets/js/create-game-component/create-game.js
@@ -1,3 +1,6 @@
+import { fetchFromServer } from "../data-connector/api-communication-abstractor.js";
+import { loadFromStorage } from "../data-connector/local-storage-abstractor.js";
+
 function init(){
 
     document.querySelector("form").addEventListener("submit", createGame);
@@ -10,25 +13,52 @@ function createGame(e){
     const visibilityList = document.querySelectorAll("input[name=visibility]");
     const amountOfPlayersList = document.querySelectorAll("input[name=players]");
 
-    const gameName = document.querySelector("#game-name").value;
+    const gameName = document.querySelector("#game-name").value.trim(); // Adding the trim() to remove the extra spaces
     const visibility = getCheckedRadioValue(visibilityList);
     const amountOfPlayers = getCheckedRadioValue(amountOfPlayersList);
 
-
-    console.log(amountOfPlayers);
+    createGameOnServer(gameName, visibility, amountOfPlayers);
 }
 
 function getCheckedRadioValue(radioButtonList){
+
     for(const radioButton of radioButtonList){
         if(radioButton.checked){
             return radioButton.value;
         }
     }
+
+}
+
+function formGameBody(playerName, gameName, visibility, amountOfPlayers){
+    
+    let body;
+
+    if( gameName !== ""){ // The there is a gameName
+        body = {
+            "gameName": gameName,
+            "numberOfPlayers": amountOfPlayers,
+            "playerName": playerName
+        }
+    } else {
+        body = {
+            "numberOfPlayers": amountOfPlayers,
+            "playerName": playerName
+        }
+    }
+
+    return body;
 }
 
 
 function createGameOnServer(gameName, visibility, amountOfPlayers){
-    
+
+    const playerName = loadFromStorage("playerName");
+    const body = formGameBody(playerName, gameName, visibility, amountOfPlayers);
+
+    console.log(body);
+
+    //fetchFromServer("/game", "POST", body)
 }
 
 init();
