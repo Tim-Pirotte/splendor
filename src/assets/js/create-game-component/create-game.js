39a3fee3bec4e69d26dee15a565b8ae1386abf23
Arne Persyn <arnepersyn8@gmail.com>
2025-03-26 13:45:12 +0100

    Redirected the creat-game.js file and cleaned up that file

--- src/assets/js/create-game-component/create-game.js
+++ src/assets/js/create-game-component/create-game.js
@@ -1,15 +1,16 @@
 import { fetchFromServer } from "../data-connector/api-communication-abstractor.js";
 import { saveToStorage, loadFromStorage } from "../data-connector/local-storage-abstractor.js";
+import { getCheckedRadioValue, formGameBody } from "./helper.js";
 
 function init(){
 
-    document.querySelector("form").addEventListener("submit", createGame);
+    document.querySelector("form").addEventListener("submit", handleCreateGameSubmit);
 
     //saveToStorage("playerName", "Johny");
 
 }
 
-function createGame(e){
+function handleCreateGameSubmit(e){
     e.preventDefault();
 
     const visibilityList = document.querySelectorAll("input[name=visibility]");
@@ -19,43 +20,15 @@ function createGame(e){
     const visibility = getCheckedRadioValue(visibilityList);
     const amountOfPlayers = parseInt(getCheckedRadioValue(amountOfPlayersList));
 
-    createGameOnServer(gameName, visibility, amountOfPlayers);
-
-    // Redirect naar de loby page
-}
-
-function getCheckedRadioValue(radioButtonList){
-
-    for(const radioButton of radioButtonList){
-        if(radioButton.checked){
-            return radioButton.value;
-        }
-    }
-
-}
-
-function formGameBody(playerName, gameName, visibility, amountOfPlayers){
-    
-    let body;
-
-    if( gameName !== ""){ // The there is a gameName
-        body = {
-            "gameName": gameName,
-            "numberOfPlayers": amountOfPlayers,
-            "playerName": playerName
-        }
-    } else {
-        body = {
-            "numberOfPlayers": amountOfPlayers,
-            "playerName": playerName
-        }
-    }
-
-    return body;
+    createGame(gameName, visibility, amountOfPlayers);
 }
 
 
-function createGameOnServer(gameName, visibility, amountOfPlayers){
+
+
+
+
+function createGame(gameName, visibility, amountOfPlayers){
 
     const playerName = loadFromStorage("playerName");
     const body = formGameBody(playerName, gameName, visibility, amountOfPlayers);
@@ -63,7 +36,9 @@ function createGameOnServer(gameName, visibility, amountOfPlayers){
     console.log(body);
 
     fetchFromServer("/games", "POST", body)
-    .then(data => console.log(`Succeded the game creation ${data}`));
+    .then(data => {
+        // Rederict to the loby page
+    });
 }
 
 init();
---
+++ src/assets/js/create-game-component/helper.js
@@ -0,0 +1,32 @@
+function getCheckedRadioValue(radioButtonList){
+
+    for(const radioButton of radioButtonList){
+        if(radioButton.checked){
+            return radioButton.value;
+        }
+    }
+
+}
+
+function formGameBody(playerName, gameName, visibility, amountOfPlayers){
+    
+    let body;
+
+    if( gameName !== ""){ // The there is a gameName
+        body = {
+            "gameName": gameName,
+            "numberOfPlayers": amountOfPlayers,
+            "playerName": playerName
+        }
+    } else {
+        body = {
+            "numberOfPlayers": amountOfPlayers,
+            "playerName": playerName
+        }
+    }
+
+    return body;
+}
+
+
+export { getCheckedRadioValue, formGameBody }
