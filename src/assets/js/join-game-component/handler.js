9c87b04f9febd2fe35d518272e196ca890ec05bd
niels clarysse <niels.clarysse@student.howest.be>
2025-03-27 21:47:51 +0100

    Renders error when player wants to join with an already existing player name (duplicate)

--- src/assets/js/join-game-component/handler.js
+++ src/assets/js/join-game-component/handler.js
@@ -8,9 +8,7 @@ function playerJoinGame(e) {
 
     if (e.target.type === "button") {
         const gameId = e.target.closest("li").dataset.gameId;
-
-        fetchFromServer(`/games/${gameId}/players/${loadFromStorage("playerName")}`, "POST")
-            .then(res => processJoinAndCreateResponse(res));
+        joinGameRequest(gameId);
     }
 }
 
@@ -18,16 +16,27 @@ function playerJoinGameById(e) {
     e.preventDefault();
 
     const gameId = document.querySelector("#game-id").value;
-
-    fetchFromServer(`/games/${gameId}/players/${loadFromStorage("playerName")}`, "POST")
-        .then(res => processJoinAndCreateResponse(res));
-
+    joinGameRequest(gameId);
 }
 
 function handleFilterChange(e) {
     e.preventDefault();
 
     renderList();
+}
+
+function joinGameRequest(gameId) {
+    fetchFromServer(`/games/${gameId}/players/${loadFromStorage("playerName")}`, "POST")
+        .then(res => processJoinAndCreateResponse(res))
+        .catch(handleDuplicateNameError);
+}
+
+function handleDuplicateNameError(err) {
+    const conflict = 409;
+
+    if (err["failure"] === conflict) {
+        document.querySelector(".error-messages").textContent = "Name is already taken";
+    }
 }
 
 export { playerJoinGame, playerJoinGameById, handleFilterChange };
