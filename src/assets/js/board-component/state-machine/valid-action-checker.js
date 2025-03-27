c47b4747f5a42f5fa8006da36be82114057f3293
Arne Persyn <arnepersyn8@gmail.com>
2025-03-27 20:26:16 +0100

    Created some functions to check if the action is valid in the current gamestate

--- src/assets/js/board-component/state-machine/valid-action-checker.js
+++ src/assets/js/board-component/state-machine/valid-action-checker.js
@@ -1,27 +1,38 @@
 import { isCurrentlyPlaying } from "../game-status-interface.js";
+import { GameState } from "./data.js";
 
 function validTokenTake() {
-    return isCurrentlyPlaying();
+    return isCurrentlyPlaying() && getGameState() === GameState.TURN_ACTION;
 }
 
 function validTokenDiscard() {
-    return isCurrentlyPlaying();
+    return isCurrentlyPlaying() && getGameState() === GameState.RETURN_GEMS;
 }
 
 function validCardBuy() {
-    return isCurrentlyPlaying();
+    return isCurrentlyPlaying() && getGameState() === GameState.TURN_ACTION;
 }
 
 function validCardReserve() {
-    return isCurrentlyPlaying();
+    return isCurrentlyPlaying() && getGameState() === GameState.TURN_ACTION && hasReservePlace();
 }
 
-function validDeckReserve() {
-    return isCurrentlyPlaying();
+function validDeckReserve() { //TODO: check if the deck is empty
+    return isCurrentlyPlaying() && getGameState() === GameState.TURN_ACTION && hasReservePlace();
 }
 
 function validNobelPick() {
-    return isCurrentlyPlaying();
+    return isCurrentlyPlaying() && getGameState() === GameState.CHOOSE_NOBEL;
+}
+
+function hasReservePlace() {
+    const amount = document.querySelectorAll("section .reserved-cards ul li").length;
+    return amount < 4;
+}
+
+
+function getGameState(){
+    return sessionStorage.getItem("gameState");
 }
 
 export { validTokenTake, validTokenDiscard, validCardBuy, validCardReserve, validDeckReserve, validNobelPick };
