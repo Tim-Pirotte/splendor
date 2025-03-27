c1011ca1e895e5adc01bf1f9d4b7cc36dbf971b6
niels clarysse <niels.clarysse@student.howest.be>
2025-03-27 11:26:57 +0100

    Changes the has game started function to accomplish the requirements for the new api feature

--- src/assets/js/general-logic/object-handler.js
+++ src/assets/js/general-logic/object-handler.js
@@ -27,7 +27,7 @@ function getPlayersObjects(gameData) {
 }
 
 function hasGameStarted(gameData){
-    return !("started" in gameData);
+    return gameData["started"];
 }
 
 export {getGameState, getGameName, getGameId, getCurrentUsersAmount, getMaxUsersAmount, getGameCreator, getPlayersObjects, hasGameStarted};
