53d6c788ee0e9d3de29754d0f78c36a4f0b5b10c
niels clarysse <niels.clarysse@student.howest.be>
2025-03-27 16:12:46 +0100

    Refactors the index.js file by adding an extra function to add event listeners

--- src/assets/js/main-menu-component/index.js
+++ src/assets/js/main-menu-component/index.js
@@ -4,15 +4,18 @@ import { renderSelectableAvatars } from "./renderer.js";
 function init() {
     renderSelectableAvatars();
     loadSavedUserInformation();
-
-    document.querySelector(".form-actions").addEventListener("click", storeUsername);
-    document.querySelector(".avatar-selector button").addEventListener("click", toggleVisibility);
-    document.querySelector(".avatar-selector section").addEventListener("click", storeAvatar);
+    addEventListeners();
 }
 
 function loadSavedUserInformation() {
     loadUsername();
     loadAvatar();
+}
+
+function addEventListeners() {
+    document.querySelector(".form-actions").addEventListener("click", storeUsername);
+    document.querySelector(".avatar-selector button").addEventListener("click", toggleVisibility);
+    document.querySelector(".avatar-selector section").addEventListener("click", storeAvatar);
 }
 
 init();
