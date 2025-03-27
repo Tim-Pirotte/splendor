7db96ebbf5a443c45e8a66aa717a08c1cc9746ca
niels clarysse <niels.clarysse@student.howest.be>
2025-03-27 10:22:04 +0100

    Fixes the game creators name being unified

--- src/assets/js/lobby-component/renderer.js
+++ src/assets/js/lobby-component/renderer.js
@@ -1,10 +1,8 @@
 import {getCurrentUsersAmount, getGameCreator, getGameId, getGameName, getMaxUsersAmount, getPlayersObjects} from "../general-logic/object-handler.js";
 
 function renderHeader(g) {
-    document.querySelector("header").insertAdjacentHTML("beforeend",
-        `<h1>Lobby</h1>
-         <h2 id="game-name">${getGameName(g)} <span id="game-id">${getGameId(g)}</span></h2>
-         <h3>Created by ${getGameCreator(g)}</h3>`);
+    document.querySelector("#game-name-id").innerHTML = `${getGameName(g)} <span$>${getGameId(g)}</span>`;
+    document.querySelector("h3").innerHTML =  `Created by ${getGameCreator(g)}`;
 }
 
 function renderPlayers(g) {
