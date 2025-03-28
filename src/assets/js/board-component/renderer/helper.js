335086adcd977a1acd1f403c36e6203b65dd68f8
Alexander Engelrelst <Alexander.engelrelst@student.howest.be>
2025-03-28 10:56:00 +0100

    reverts to previous state

--- src/assets/js/board-component/renderer/helper.js
+++ src/assets/js/board-component/renderer/helper.js
@@ -36,12 +36,13 @@ function formatNumber(number) {
   return number.toString().padStart(2, '0');
 }
 
-function renderCard($container, points, bonus, costs, cardName) {
+function renderCard($container, points, bonus, costs, index, level) {
   const $numberedItemTemplate = document.querySelector("#numbered-item-template");
   const $card = document.querySelector("#card-template").content.firstElementChild.cloneNode(true);
 
   $card.querySelector(".points").textContent = points;
-  $card.dataset.name = cardName;
+  $card.dataset.index = index;
+  $card.dataset.level = level;
 
   const $cardCost = $card.querySelector(".cost");
 
