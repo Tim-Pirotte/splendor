cea5c4409f163e4a1951afc0db6834452faa3437
Tristan Joos <tristan.joos@outlook.be>
2025-03-28 14:24:25 +0100

    Fixes isues with f elses and enters

--- src/assets/js/board-component/token/token-handler.js
+++ src/assets/js/board-component/token/token-handler.js
@@ -2,21 +2,19 @@ import { takeGemsRequest } from "./request-handler.js";
 import { setActionButtonState } from "../game-status-interface.js";
 import { MIN_TOKENS_FOR_PICKING_TWO } from "./config.js";
 
-
 function canGetToken(tokenType, amount , $actionButtonData) {
     if(checkIfTokenIsEmpty($actionButtonData.token1)){
         return (tokenType !== "Gold") && amount >= MIN_TOKENS_FOR_PICKING_TWO;
     }
    return false;
-
 }
+
 function giveTokenThatAlreadySelected(tokenType , actionButtonData){
     const token1 = 1;
     const token2 = 2;
     const token3 = 3;
 
     if (tokenType === actionButtonData.token1) {
-
         return token1;
     }
     if (tokenType === actionButtonData.token2) {
@@ -27,17 +25,15 @@ function giveTokenThatAlreadySelected(tokenType , actionButtonData){
     }
     return null;
 }
+
 function checkIfTokenAlreadySelected(tokenType , actionButtonData) {
    return (giveTokenThatAlreadySelected(tokenType , actionButtonData) !== null);
-
-
-
 }
+
 function removeToken(token , actionButtonData) {
     const token1 = 1;
     const token2 = 2;
     const token3 = 3;
-
 
     if (token === token1){
         actionButtonData.token1 = "";
@@ -52,39 +48,31 @@ function removeToken(token , actionButtonData) {
 
 function checkIfTokenIsEmpty(token) {
     return (token === undefined || token === "");
-
 }
 function selectToken(e) {
-    if (e.target.tagName.toLowerCase() === "img" ) {
+    if (e.target.tagName.toLowerCase() === "img") {
         const $selectedToken = e.target.closest("li");
         const tokenType = $selectedToken.dataset.type;
         const $actionButton = document.querySelector(".action-button");
         const $actionButtonData = $actionButton.dataset;
 
-        if (canGetToken(tokenType, $selectedToken.dataset.amount , $actionButtonData)) {
+        if (canGetToken(tokenType, $selectedToken.dataset.amount, $actionButtonData)) {
             setActionButtonState("Take two", "processTakeTokenClick", {token1: tokenType});
-
-        } else if (checkIfTokenAlreadySelected(tokenType, $actionButtonData)) {
+        }
+        if (checkIfTokenAlreadySelected(tokenType, $actionButtonData)) {
             removeToken(giveTokenThatAlreadySelected(tokenType, $actionButtonData), $actionButtonData);
-        } else if (tokenType !== "Gold" && $selectedToken.dataset.amount >= 1 && !checkIfTokenAlreadySelected(tokenType, $actionButton)) {
-
+        }
+        if (tokenType !== "Gold" && $selectedToken.dataset.amount >= 1 && !checkIfTokenAlreadySelected(tokenType, $actionButton)) {
             if (checkIfTokenIsEmpty($actionButtonData.token1)) {
                 setActionButtonState("select two more gems", "processTakeTokenClick", {token1: tokenType});
-
-            } else if (checkIfTokenIsEmpty($actionButtonData.token2)) {
-                setActionButtonState("select one more gems", "processTakeTokenClick", {token2: tokenType});
-
-            } else if (checkIfTokenIsEmpty($actionButtonData.token3)) {
-                setActionButtonState("Take three gems", "processTakeTokenClick", {token3: tokenType});
-
-            } else {
-                //purely here so sonar doesn't complain
             }
-        } else {
-            //purely here so sonar doesn't complain
+            if (checkIfTokenIsEmpty($actionButtonData.token2)) {
+                setActionButtonState("select one more gems", "processTakeTokenClick", {token2: tokenType});
+            }
+            if (checkIfTokenIsEmpty($actionButtonData.token3)) {
+                setActionButtonState("Take three gems", "processTakeTokenClick", {token3: tokenType});
+            }
         }
-    }else{
-        //Again sonar complained
     }
 }
 
@@ -95,10 +83,8 @@ function processTakeTokenClick(e) {
     if ($actionButton.textContent !== "Take two") {
         const body = [actionButtonData.token1, actionButtonData.token2 , actionButtonData.token3];
         takeGemsRequest(body , "");
-
     } else{
         takeGemsRequest(actionButtonData.token1 , "takeTwo");
-
     }
 }
 
@@ -112,7 +98,6 @@ function updateTokens(res) {
      const $amountText = $token.querySelector("p");
 
      $amountText.textContent = `${$token.dataset.amount}${$amountText.textContent.substring(beginIndexAmountText, endIndexAmountText)}`;
-
     }
 }
 
