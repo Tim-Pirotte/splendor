2b38027a8baee957fa5c90e9eb5522b1c7c086c2
Tim Pirotte <timpirotte@outlook.com>
2025-03-28 08:57:38 +0100

    Sends a request to the server to take a noble

--- src/assets/js/board-component/nobles/nobles-handler.js
+++ src/assets/js/board-component/nobles/nobles-handler.js
@@ -1,13 +1,31 @@
 import {setActionButtonState} from "../game-status-interface.js";
+import {fetchFromServer} from "../../data-connector/api-communication-abstractor.js";
+import {loadFromStorage} from "../../data-connector/local-storage-abstractor.js";
 
 function selectNoble(e) {
   const $selectedNoble = e.target.closest("li");
   console.log($selectedNoble);
-  setActionButtonState("Take Noble", "processTakeNoble", {});
+  setActionButtonState("Take Noble", "processTakeNoble", {name: $selectedNoble.dataset.name});
 }
 
 function processTakeNoble() {
-  console.log("test");
+  const actionButton = document.querySelector(".action-button");
+  console.log(actionButton.dataset.name);
+  fetchFromServer(
+    `/games/${loadFromStorage("gameId")}/players/${loadFromStorage("playerName")}/nobles`,
+  "POST",
+    {
+      name: actionButton.dataset.name,
+      prestigePoints: 0,
+      neededBonuses: {
+        Emerald: 0,
+        Sapphire: 0,
+        Ruby: 0,
+        Diamond: 0,
+        Onyx: 0,
+        Gold: 0
+      }
+    }).then(res => console.log(res));
 }
 
 export { selectNoble, processTakeNoble };
