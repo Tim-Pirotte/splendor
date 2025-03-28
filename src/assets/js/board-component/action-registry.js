bc3f394ab7e44fe0f60627f0fc94c83b3996205d
Tim Pirotte <timpirotte@outlook.com>
2025-03-28 07:59:01 +0100

    Adds initial file structure for noble logic

--- src/assets/js/board-component/action-registry.js
+++ src/assets/js/board-component/action-registry.js
@@ -2,6 +2,7 @@ import {processTakeTokenClick} from "./token/token-handler.js";
 
 const ACTION_REGISTRY = {
   processTakeTokenClick: () => processTakeTokenClick(),
+  processTakeNobleClick: () => processTakeNoble()
 };
 
 export {ACTION_REGISTRY};
