1b0ecca12d491af5f3d8d324280c94dabd2261bb
Tim Pirotte <timpirotte@outlook.com>
2025-03-28 11:19:36 +0100

    Saves type in tokens

--- src/assets/js/board-component/nobles/nobles-handler.js
+++ src/assets/js/board-component/nobles/nobles-handler.js
@@ -13,8 +13,18 @@ function selectNoble(e) {
 
 function canSelectNoble(nobleName) {
   const noble = getNobleByName(nobleName);
-
+  const playerBonuses = getPlayerBonuses();
+  console.log(playerBonuses);
   return false;
+}
+
+function getPlayerBonuses() {
+  const bonuses = {};
+  document.querySelectorAll(".player-tokens ul").forEach(token => {
+    bonuses[token.dataset.type] = token.dataset.amount;
+  })
+
+  return bonuses;
 }
 
 function processTakeNoble() {
