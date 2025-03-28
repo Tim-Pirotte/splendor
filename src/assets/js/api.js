bb75142af61048a0dcb3e3f360c6e8114ea7a339
niels clarysse <niels.clarysse@student.howest.be>
2025-03-28 11:15:03 +0100

    Adds getGames function to the api.js file

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -1,1 +1,13 @@
-// all fetches will happen here!
+import {fetchFromServer} from "./data-connector/api-communication-abstractor.js";
+import {loadFromStorage} from "./data-connector/local-storage-abstractor.js";
+
+/* Game Management */
+function getGames(hasStarted = "") {
+    if (hasStarted !== "") {
+        return fetchFromServer(`/games?started=${hasStarted}`);
+    } else {
+        return fetchFromServer("/games");
+    }
+}
+
+export { getGames };
