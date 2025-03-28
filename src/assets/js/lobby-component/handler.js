328e6d2d82b16deb19ecd0cfdfff26c7450018d5
Tim Pirotte <timpirotte@outlook.com>
2025-03-28 15:15:37 +0100

    Adds polling to the lobby page

--- src/assets/js/lobby-component/handler.js
+++ src/assets/js/lobby-component/handler.js
@@ -2,6 +2,7 @@ import {hasGameStarted} from "../utils/game-object-handler.js";
 import { fetchFromServer } from "../data-connector/api-communication-abstractor.js";
 import { loadFromStorage } from "../data-connector/local-storage-abstractor.js";
 import { renderHeader, renderPlayerCount, renderPlayers } from "./renderer.js";
+import {POLLING_TIME_OUT} from "../config.js";
 
 function loadLobbyInformation() {
     fetchFromServer(`/games/${loadFromStorage("gameId")}`)
@@ -10,10 +11,15 @@ function loadLobbyInformation() {
                 renderHeader(gameObject);
                 renderPlayers(gameObject);
                 renderPlayerCount(gameObject);
+                startPolling();
             } else {
                 location.href = `./board.html`;
             }
         });
+}
+
+function startPolling() {
+  setTimeout(loadLobbyInformation, POLLING_TIME_OUT);
 }
 
 export { loadLobbyInformation };
