4151bfd80a303717b5fe8fa0d339a5c812b323fa
Arne Persyn <arnepersyn8@gmail.com>
2025-03-28 10:33:26 +0100

    Added an empty skipTurn function

--- src/assets/js/board-component/action-registry.js
+++ src/assets/js/board-component/action-registry.js
@@ -2,6 +2,7 @@ import {processTakeTokenClick} from "./token/token-handler.js";
 
 const ACTION_REGISTRY = {
   processTakeTokenClick: () => processTakeTokenClick(),
+  skipTurn: () => {}
 };
 
 export {ACTION_REGISTRY};
