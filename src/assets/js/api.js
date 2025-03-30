a9ee78402192ce6b9effa7d22f338bc5c262fe12
Tim Pirotte <timpirotte@outlook.com>
2025-03-30 16:33:55 +0200

    Restore the api.js to its original state

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -16,13 +16,11 @@ function createGame(requestBody) {
 
 function getGame() {
     const gameId = loadFromStorage("gameId");
-
     return fetchFromServer(`/games/${gameId}`);
 }
 
 function joinGame(gameId) {
     const playerName = loadFromStorage("playerName");
-
     return fetchFromServer(`/games/${gameId}/players/${playerName}`, "POST");
 }
 
@@ -30,35 +28,30 @@ function joinGame(gameId) {
 function takeTokens(requestBody) {
     const gameId = loadFromStorage("gameId");
     const playerName = loadFromStorage("playerName");
-
     return fetchFromServer(`/games/${gameId}/players/${playerName}/tokens`, "PATCH", requestBody);
 }
 
 function buyCard(requestBody) {
     const gameId = loadFromStorage("gameId");
     const playerName = loadFromStorage("playerName");
-
     return fetchFromServer(`/games/${gameId}/players/${playerName}/developments`, "POST", requestBody);
 }
 
 function reserveCard(requestBody) {
     const gameId = loadFromStorage("gameId");
     const playerName = loadFromStorage("playerName");
-
     return fetchFromServer(`/games/${gameId}/players/${playerName}/reserve`, "POST", requestBody);
 }
 
 function buyReserveCard(developmentName, requestBody) {
     const gameId = loadFromStorage("gameId");
     const playerName = loadFromStorage("playerName");
-
     return fetchFromServer(`/games/${gameId}/players/${playerName}/reserve/${developmentName}`, "POST", requestBody);
 }
 
 function takeNobels(requestBody) {
     const gameId = loadFromStorage("gameId");
     const playerName = loadFromStorage("playerName");
-
     return fetchFromServer(`/games/${gameId}/players/${playerName}/nobels`, "POST", requestBody);
 }
 
@@ -67,12 +60,4 @@ function getGemsList() {
     return fetchFromServer("/gems");
 }
 
-function getNoblesList() {
-    return fetchFromServer("/nobles");
-}
-
-function getDevelopmentsList() {
-    return fetchFromServer("/developments");
-}
-
-export { getGames, createGame, getGame, joinGame, takeTokens, buyCard, reserveCard, buyReserveCard, takeNobels, getGemsList, getNoblesList, getDevelopmentsList };
+export { getGames, createGame, getGame, joinGame, takeTokens, buyCard, reserveCard, buyReserveCard, takeNobels, getGemsList };
