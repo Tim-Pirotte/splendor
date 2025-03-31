80480a1ce84b0dfc2a3a28aaf8a535f7b428a726
Tim Pirotte <timpirotte@outlook.com>
2025-03-31 09:51:42 +0200

    Sorts the data.js file for binary search

--- src/assets/js/board-component/board.js
+++ src/assets/js/board-component/board.js
@@ -3,18 +3,40 @@ import { initGameStatusInterface } from "./game-status-interface.js";
 import { tokenInit } from "./token/token.js";
 import { buyInit } from "./buy/buy.js";
 import { noblesInit } from "./nobles/nobles.js";
+import {DEVELOPMENT_CARDS, NOBLES} from "./data.js";
 
 function init() {
     getGems();
     updateGameData();
     initGameStatusInterface();
     initializeActions();
+    sortAndPrint(DEVELOPMENT_CARDS, "name")
 }
 
 function initializeActions() {
     tokenInit();
     noblesInit();
     buyInit();
+}
+
+function sortAndPrint(objects, attribute) {
+    if (!Array.isArray(objects) || objects.length === 0) {
+        console.log("Invalid input: Provide a non-empty array of objects.");
+        return;
+    }
+
+    if (!(attribute in objects[0])) {
+        console.log(`Invalid attribute: '${attribute}' not found in objects.`);
+        return;
+    }
+
+    const sortedObjects = [...objects].sort((a, b) => {
+        if (a[attribute] < b[attribute]) return -1;
+        if (a[attribute] > b[attribute]) return 1;
+        return 0;
+    });
+
+    console.log(JSON.stringify(sortedObjects, null, 4));
 }
 
 init();
