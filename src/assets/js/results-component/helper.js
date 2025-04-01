0a514e0ea57ed3f7edd5d35ec8bedd03f7109e5d
niels clarysse <niels.clarysse@student.howest.be>
2025-04-01 14:10:52 +0200

    Fixes linter issues

--- src/assets/js/results-component/helper.js
+++ src/assets/js/results-component/helper.js
@@ -1,26 +1,21 @@
 import * as API from "../api.js";
-import {sumObjectValues} from "../utils/game-object-handler.js";
+import { sumObjectValues } from "../utils/game-object-handler.js";
 
-function getResults() {
-    return API.getGame()
-        .then(gameData => filterResults(gameData));
-}
-
-function filterResults(gameData) {
+function getSortedResults() {
     return API.getGame()
         .then(({ players }) => {
             const results = players.map(player => ({
                 name: player.name,
                 points: player["totalPrestigePoints"],
-                amountOfBonuses: getAmountOfBonuses(player)
+                amountOfBonuses: getAmountOfBonuses(player),
             }))
-            .sort((a, b) => b.points - a.points || b.amountOfBonuses - a.amountOfBonuses);
+                .sort((a, b) => b.points - a.points || b.amountOfBonuses - a.amountOfBonuses);
 
             const topPlayerScore = results[0].points;
             const topPlayerBonuses = results[0].amountOfBonuses;
 
             return results.map(player => ({
-                ...player, isWinner : getWinner(player, topPlayerScore, topPlayerBonuses)
+                ...player, isWinner: getWinner(player, topPlayerScore, topPlayerBonuses),
             }));
         });
 }
@@ -33,4 +28,4 @@ function getWinner(player, topPlayerScore, topPlayerBonuses) {
     return player.points === topPlayerScore && player.amountOfBonuses === topPlayerBonuses;
 }
 
-export { getResults };
+export { getSortedResults };
