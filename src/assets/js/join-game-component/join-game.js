7ba66f52776635c1e76ab07f3f7a21571da0f016
niels clarysse <niels.clarysse@student.howest.be>
2025-04-04 15:28:35 +0200

    Adds the functionality to the backbutton

--- src/assets/js/join-game-component/join-game.js
+++ src/assets/js/join-game-component/join-game.js
@@ -1,15 +1,21 @@
 import { loadFromStorage } from "../data-connector/local-storage-abstractor.js";
+import { locateToMainMenu } from "./helper.js";
 import { renderPlayerInfo, renderPublicGames } from "./renderer.js";
 
 function joinInit() {
     setupUI();
+    setupEventListeners();
 }
 
 function setupUI() {
-    if (!loadFromStorage("playerName")) location.href = `./../index.html`;
-    if (!loadFromStorage("avatar")) location.href = `./../index.html`;
+    if (!loadFromStorage("playerName")) locateToMainMenu();
+    if (!loadFromStorage("avatar")) locateToMainMenu();
     renderPlayerInfo();
     renderPublicGames();
+}
+
+function setupEventListeners() {
+    document.querySelector("#back-button").addEventListener("click", locateToMainMenu);
 }
 
 joinInit();
