56fefe841fbf9acbcee3a2a7aacd3d79f15f24b5
Tim Pirotte <timpirotte@outlook.com>
2025-04-24 19:34:14 +0200

    Adds new files for the animation system

---
+++ src/assets/js/board-component/animation-component/animation-handler.js
@@ -0,0 +1,4 @@
+/* This animation system is based on the following resource:
+   https://thomaswilburn.github.io/viz-book/perf-flip.html
+   It uses the FLIP technique (First, Last, Invert, Play) */
+
