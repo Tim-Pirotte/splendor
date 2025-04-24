1713bdc2f1d78ac8570dd382843dffe13f61de20
Tim Pirotte <timpirotte@outlook.com>
2025-04-24 20:47:09 +0200

    Adds a proper templating system

--- src/assets/js/board-component/animation-component/animation-handler.js
+++ src/assets/js/board-component/animation-component/animation-handler.js
@@ -21,17 +21,79 @@ function getInvertedPositionSize(sourcePosition, targetPosition) {
 }
 
 function startTargetAnimation($targetNode, invertedPosSize, animation) {
-  $targetNode.animate(
-    insertVariables(animation.keyFrames, invertedPosSize),
+  insertVariablesIntoKeyframes(animation, invertedPosSize);
+
+  const animationPlayer = $targetNode.animate(
+    animation.keyFrames,
     {
       duration: animation.duration,
       easing: animation.easeFunction,
       },
   );
+
+  animationPlayer.addEventListener("finish", () => cleanupAnimation($targetNode, animation.keyFrames));
 }
 
-function insertVariables(targetString, varsToInsert) {
-  for (const [varName, value] of Object.entries(varsToInsert)) {
-    targetString.replaceAll(`{{ ${varName} }}`, value);
+function insertVariablesIntoKeyframes(animation, invertedPosSize) {
+  for (const keyFrame of animation.keyFrames) {
+    insertVariablesIntoKeyframe(keyFrame, invertedPosSize);
   }
+}
+
+function cleanupAnimation($targetNode, keyframes) {
+  for (const keyframe of keyframes) {
+    for (const property of Object.keys(keyframe)) {
+      $targetNode.removeProperty(property);
+    }
+  }
+}
+
+function insertVariablesIntoKeyframe(keyFrame, invertedPosSize) {
+  for (const [key, value] of Object.entries(keyFrame)) {
+    if (typeof value === "string") {
+      keyFrame[key] = insertVariables(value, invertedPosSize);
+    }
+  }
+}
+
+function insertVariables(targetString, varsToInsert, templateStartSymbol = "{{", templateEndSymbol = "}}") {
+  let result = "";
+  let currentPos = 0;
+
+  while (currentPos !== targetString.length) {
+    const nextTemplatePos = getNextTemplatePos(targetString, currentPos, templateStartSymbol);
+
+    if (nextTemplatePos === -1) {
+      result += targetString.slice(currentPos);
+      break;
+    }
+
+    result += targetString.slice(currentPos, nextTemplatePos);
+
+    const templateEndPos = getTemplateEndPos(targetString, nextTemplatePos, templateEndSymbol);
+    const varToInsert = getVarToInsert(targetString, nextTemplatePos, templateStartSymbol, templateEndPos, varsToInsert);
+
+    result += varsToInsert[varToInsert];
+    currentPos = templateEndPos + templateEndPos.length;
+  }
+
+  return result;
+}
+
+function getNextTemplatePos(targetString, currentPos, templateStartSymbol) {
+  return targetString.indexOf(templateStartSymbol, currentPos);
+}
+
+function getTemplateEndPos(targetString, nextTemplatePos, templateEndSymbol) {
+  const templateEndPos = targetString.indexOf(templateEndSymbol, nextTemplatePos);
+  if (templateEndPos === -1) throw new SyntaxError(`Unclosed template tag at ${nextTemplatePos}`);
+
+  return templateEndPos;
+}
+
+function getVarToInsert(targetString, nextTemplatePos, templateStartSymbol, templateEndPos, varsToInsert) {
+  const varToInsert = targetString.slice(nextTemplatePos + templateStartSymbol.length, templateEndPos).trim();
+  if (!(varToInsert in varsToInsert)) throw new SyntaxError(`Variable ${varToInsert} does not exist`);
+
+  return varToInsert;
 }
