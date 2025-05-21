46009d69f5b8cd15c73267ec080e9c93c26cf59a
Arne Persyn <arnepersyn8@gmail.com>
2025-05-21 09:48:57 +0200

    Completed the check function to test the username

--- src/assets/js/main-menu-component/validator.js
+++ src/assets/js/main-menu-component/validator.js
@@ -1,5 +1,6 @@
 function validatePlayerName(playerName) {
-
+    const playerNameRegex = /^[a-zA-Z0-9]{1,16}$/;
+    return playerNameRegex.test(playerName) && playerName.length <= 16;  
 }
 
 export { validatePlayerName };
