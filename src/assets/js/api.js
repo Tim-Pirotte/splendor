c7701524cb1f5625e708a98b89dd6a0d513bb08c
Tristan Joos <tristan.joos@outlook.be>
2025-05-22 11:29:06 +0200

    makes createBotGame

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -4,6 +4,7 @@ import { checkCompatibility } from "./server-version-component/server-version.js
 import { NPC_SUFFIX } from "./config.js";
 import { handleGameDataError } from "./board-component/game-data-handler.js";
 import { locateToMainMenu } from "./utils/data-handler.js";
+import {createGameWithBody, getCheckedRadioButtonValue} from "./create-game-component/helper";
 
 /* Game Management */
 function getGames(hasStarted = "") {
@@ -41,6 +42,23 @@ function joinGame(gameId, playerName, spectatingEnabled, forfeit) {
 
         return fetchFromServer(`/games/${gameId}/players/${playerName}`, "POST", body);
     });
+}
+
+function createBotGame(){
+    const level = 3;
+    const numberOfPlayers = 2;
+    let gameId;
+    const requestBody = {
+        playerName: level + NPC_SUFFIX,
+        gameName: "BotGame",
+        numberOfPlayers: numberOfPlayers,
+        returnExcessTokensRequired: true,
+        pickNobleRequired: true,
+    };
+    createGame(requestBody).then(response => {
+        gameId =   response["gameId"];
+    });
+    return gameId;
 }
 
 function joinBot(level) {
@@ -99,4 +117,5 @@ export {
     getApiInfo,
     leaveGame,
     joinBot,
+    createBotGame,
 };
