b099a3b600f8620090c2582339315fd08b4aeb59
Arne Persyn <arnepersyn8@gmail.com>
2025-05-22 15:16:03 +0200

    Compatibility is working until the board page

--- src/assets/js/board-component/renderer/renderer.js
+++ src/assets/js/board-component/renderer/renderer.js
@@ -7,18 +7,25 @@ import {
 import { renderHistory, renderOtherPlayers } from "./sidebar-renderer.js";
 import { renderBoardTokens, renderCards, renderNobles } from "./board-renderer.js";
 import { GEMS } from "../data.js";
+import { checkCompatibility } from "../../server-version-component/server-version.js";
 
 function renderPage(gameData) {
-    renderPlayerProfile(gameData["players"][0]["name"], gameData["spectators"]);
-    renderAmountOfSpectators(gameData["spectators"]);
-    renderGameStatusMessage(gameData["currentPlayer"]);
-    renderOtherPlayers(gameData["players"], gameData["currentPlayer"]);
-    renderHistory(gameData["history"]);
-    renderBoardTokens(gameData["unclaimedTokens"], gameData["players"].length);
-    renderClientPlayer(gameData["players"], GEMS);
-    // Has to be rendered after the client player so that the player wallet exists
-    renderCards(gameData["market"]);
-    renderNobles(gameData["unclaimedNobles"]);
+    checkCompatibility(2)
+        .then(compatible => {
+            if (compatible) {
+                renderAmountOfSpectators(gameData["spectators"]);
+            }
+
+            renderPlayerProfile(gameData["players"][0]["name"], gameData["spectators"]);
+            renderGameStatusMessage(gameData["currentPlayer"]);
+            renderOtherPlayers(gameData["players"], gameData["currentPlayer"]);
+            renderBoardTokens(gameData["unclaimedTokens"], gameData["players"].length);
+            renderClientPlayer(gameData["players"], GEMS);
+            renderHistory(gameData["history"]);
+            // Has to be rendered after the client player so that the player wallet exists
+            renderCards(gameData["market"]);
+            renderNobles(gameData["unclaimedNobles"]);
+        })
 }
 
 export { renderPage };
